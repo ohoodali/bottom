70dd0f34ce95898a451104035cdd98b199d0b22e
ohood <msohood@gmail.com>
2023-09-22 20:33:27 +0300

    my second

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,9 @@ class NavigationBarApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: NavigationExample());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: NavigationExample());
   }
 }
 
@@ -88,8 +90,9 @@ class _NavigationExampleState extends State<NavigationExample> {
       ),
       body:Center(
         /** Card Widget **/
-        child: Column(
-          children: [
+
+        child: ListView(
+          children: <Widget> [
             Card(
               elevation: 50,
               shadowColor: Colors.black,
@@ -114,7 +117,7 @@ class _NavigationExampleState extends State<NavigationExample> {
                         height: 10,
                       ), //SizedBox
                       Text(
-                        'GeeksforGeeks',
+                        'My WebSite',
                         style: TextStyle(
                           fontSize: 30,
                           color: Colors.green[900],
@@ -125,7 +128,7 @@ class _NavigationExampleState extends State<NavigationExample> {
                         height: 10,
                       ), //SizedBox
                       const Text(
-                        'GeeksforGeeks is a computer science portal for geeks at geeksforgeeks.org. It contains well written, well thought and well explained computer science and programming articles, quizzes, projects, interview experiences and much more!!',
+                        'It contains well written, well thought and well explained computer science and programming articles, quizzes, projects, interview experiences and much more!!',
                         style: TextStyle(
                           fontSize: 15,
                           color: Colors.green,
@@ -183,7 +186,7 @@ class _NavigationExampleState extends State<NavigationExample> {
                         height: 10,
                       ), //SizedBox
                       Text(
-                        'GeeksforGeeks',
+                        'My WebSite',
                         style: TextStyle(
                           fontSize: 30,
                           color: Colors.green[900],
@@ -194,7 +197,214 @@ class _NavigationExampleState extends State<NavigationExample> {
                         height: 10,
                       ), //SizedBox
                       const Text(
-                        'GeeksforGeeks is a computer science portal for geeks at geeksforgeeks.org. It contains well written, well thought and well explained computer science and programming articles, quizzes, projects, interview experiences and much more!!',
+                        ' It contains well written, well thought and well explained computer science and programming articles, quizzes, projects, interview experiences and much more!!',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.green,
+                        ), //Textstyle
+                      ), //Text
+                      const SizedBox(
+                        height: 10,
+                      ), //SizedBox
+                      SizedBox(
+                        width: 100,
+
+                        child: ElevatedButton(
+                          onPressed: () => 'Null',
+                          style: ButtonStyle(
+                              backgroundColor:
+                              MaterialStateProperty.all(Colors.green)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(4),
+                            child: Row(
+                              children: const [
+                                Icon(Icons.touch_app),
+                                Text('Visit')
+                              ],
+                            ),
+                          ),
+                        ),
+
+                      ) //SizedBox
+                    ],
+                  ), //Column
+                ), //Padding
+              ), //SizedBox
+            ),
+            Card(
+              elevation: 50,
+              shadowColor: Colors.black,
+              color: Colors.greenAccent[100],
+              child: SizedBox(
+                width: 300,
+                height: 500,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.green[500],
+                        radius: 108,
+                        child: const CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              "https://media.geeksforgeeks.org/wp-content/uploads/20210101144014/gfglogo.png"), //NetworkImage
+                          radius: 100,
+                        ), //CircleAvatar
+                      ), //CircleAvatar
+                      const SizedBox(
+                        height: 10,
+                      ), //SizedBox
+                      Text(
+                        'My WebSite',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.green[900],
+                          fontWeight: FontWeight.w500,
+                        ), //Textstyle
+                      ), //Text
+                      const SizedBox(
+                        height: 10,
+                      ), //SizedBox
+                      const Text(
+                        ' It contains well written, well thought and well explained computer science and programming articles, quizzes, projects, interview experiences and much more!!',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.green,
+                        ), //Textstyle
+                      ), //Text
+                      const SizedBox(
+                        height: 10,
+                      ), //SizedBox
+                      SizedBox(
+                        width: 100,
+
+                        child: ElevatedButton(
+                          onPressed: () => 'Null',
+                          style: ButtonStyle(
+                              backgroundColor:
+                              MaterialStateProperty.all(Colors.green)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(4),
+                            child: Row(
+                              children: const [
+                                Icon(Icons.touch_app),
+                                Text('Visit')
+                              ],
+                            ),
+                          ),
+                        ),
+
+                      ) //SizedBox
+                    ],
+                  ), //Column
+                ), //Padding
+              ), //SizedBox
+            ),
+            Card(
+              elevation: 50,
+              shadowColor: Colors.black,
+              color: Colors.greenAccent[100],
+              child: SizedBox(
+                width: 300,
+                height: 500,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.green[500],
+                        radius: 108,
+                        child: const CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              "https://media.geeksforgeeks.org/wp-content/uploads/20210101144014/gfglogo.png"), //NetworkImage
+                          radius: 100,
+                        ), //CircleAvatar
+                      ), //CircleAvatar
+                      const SizedBox(
+                        height: 10,
+                      ), //SizedBox
+                      Text(
+                        'My WebSite',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.green[900],
+                          fontWeight: FontWeight.w500,
+                        ), //Textstyle
+                      ), //Text
+                      const SizedBox(
+                        height: 10,
+                      ), //SizedBox
+                      const Text(
+                        ' It contains well written, well thought and well explained computer science and programming articles, quizzes, projects, interview experiences and much more!!',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.green,
+                        ), //Textstyle
+                      ), //Text
+                      const SizedBox(
+                        height: 10,
+                      ), //SizedBox
+                      SizedBox(
+                        width: 100,
+
+                        child: ElevatedButton(
+                          onPressed: () => 'Null',
+                          style: ButtonStyle(
+                              backgroundColor:
+                              MaterialStateProperty.all(Colors.green)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(4),
+                            child: Row(
+                              children: const [
+                                Icon(Icons.touch_app),
+                                Text('Visit')
+                              ],
+                            ),
+                          ),
+                        ),
+
+                      ) //SizedBox
+                    ],
+                  ), //Column
+                ), //Padding
+              ), //SizedBox
+            ),
+            Card(
+              elevation: 50,
+              shadowColor: Colors.black,
+              color: Colors.greenAccent[100],
+              child: SizedBox(
+                width: 300,
+                height: 500,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.green[500],
+                        radius: 108,
+                        child: const CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              "https://media.geeksforgeeks.org/wp-content/uploads/20210101144014/gfglogo.png"), //NetworkImage
+                          radius: 100,
+                        ), //CircleAvatar
+                      ), //CircleAvatar
+                      const SizedBox(
+                        height: 10,
+                      ), //SizedBox
+                      Text(
+                        'My WebSite',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.green[900],
+                          fontWeight: FontWeight.w500,
+                        ), //Textstyle
+                      ), //Text
+                      const SizedBox(
+                        height: 10,
+                      ), //SizedBox
+                      const Text(
+                        ' It contains well written, well thought and well explained computer science and programming articles, quizzes, projects, interview experiences and much more!!',
                         style: TextStyle(
                           fontSize: 15,
                           color: Colors.green,
@@ -230,5 +440,6 @@ class _NavigationExampleState extends State<NavigationExample> {
             ),
           ],
         ), //Card
+
       ), //Center
     );}} //Scaffold
